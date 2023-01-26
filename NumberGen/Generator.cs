e140079d60e14ee3112b964b5d46d87cc3d996fa
anya ebube <anyaebube4im@outlook.com>
2023-01-26 11:06:34 +0100

    Add project files.

---
+++ NumberGen/Generator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGen
+{
+    internal class  Generator
+    {
+
+        List<int> GeneratedNumbers = new List<int>();
+        int number;
+        Random randomNumber = new Random(1);
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter G to Generate and Y to exit!");
+
+                string Input = Console.ReadLine().ToString();
+
+                switch (Input.ToUpper())
+                {
+                    case "G":
+                        Console.WriteLine(Generate());
+                        break;
+                    case "Y":
+                        return;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input!");
+                        break;
+                }
+            }
+        }
+
+        int Generate()
+        {
+                
+          
+            number = randomNumber.Next(1,27);
+            if (!GeneratedNumbers.Contains(number))
+            {
+                GeneratedNumbers.Add(number);
+                return number;
+            }
+            else
+            {
+                return Generate();
+            }
+
+           // return 0;
+
+
+        }
+    }
+}
---
+++ NumberGen/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NumberGen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generator Begins");
+            Generator gen = new Generator();
+            gen.Run();
+        }
+    }
+}
